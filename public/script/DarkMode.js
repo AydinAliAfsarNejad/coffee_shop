0471436647ceb06c5cf86134497e7388405b7e3f
aydin <aidinafsary@gmail.com>
2025-04-22 20:45:38 +0330

    mobile responsive update beta 0.2

--- public/script/DarkMode.js
+++ public/script/DarkMode.js
@@ -16,3 +16,21 @@ function removeItem(theme) {
 }
 
 removeItem("theme");
+
+
+const toggleThemeBtn2 = document.querySelector("#toggle-theme2");
+toggleThemeBtn2.addEventListener("click" , () => {
+    if (localStorage.theme === "dark"){
+        document.documentElement.classList.remove("dark");
+        localStorage.theme = "light";
+    } else {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme" , "dark");
+    }
+})
+
+function removeItem2(theme) {
+
+}
+
+removeItem2("theme");
--- public/script/script.js
+++ public/script/script.js
@@ -2,14 +2,37 @@
 
 // document
 let $ = document
-let loadElem = $.getElementById("loader");
+let $$ = $.querySelector
+let shopItem = $.querySelector(".SubMenu")
+let SubMenu = $.querySelector(".ShopItems")
+let navIconOpenBtn = $.querySelector("#nav-icon-openBtn")
+let mobileHeader = $.querySelector("#mobile-header")
+let navMenuCloseBtn = $.querySelector("#nav-menu-closeBtn")
 
-// loader
 
-window.addEventListener("load" , (event) => {
-    loadElem.classList.add("hide")
-    $.body.style = "block"
-    console.log(event)
-    
+// scripts
+
+// toggle shop items
+shopItem.addEventListener("click", (hover) => {
+
+    hover.currentTarget.parentElement.classList.toggle("text-orange-300" )
+    hover.currentTarget.parentElement.classList.toggle("transition-all" )
+    hover.currentTarget.parentElement.classList.toggle("duration-200ms" )
+
+    SubMenu.classList.toggle("ShopItemOpen")
+})
+
+//open nav Menu
+navIconOpenBtn.addEventListener("click", () => {
+
+    mobileHeader.classList.remove("-right-64")
+    mobileHeader.classList.add("right-0")
+})
+
+// close nav Menu
+navMenuCloseBtn.addEventListener("click", () => {
+    // console.log("close")
+
+    mobileHeader.classList.remove("right-0")
+    mobileHeader.classList.add("-right-64")
 });
-
